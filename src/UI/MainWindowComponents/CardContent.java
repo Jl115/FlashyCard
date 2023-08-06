4b5791cfb31d6df61e8a2e08ff917aa555a7b8d1
Jl115 <106701193+Jl115@users.noreply.github.com>
2023-08-06 15:10:29 +0200

    File cleanup and implementation of random postion textAreas and buttons groups

--- src/UI/MainWindowComponents/CardContent.java
+++ src/UI/MainWindowComponents/CardContent.java
@@ -8,45 +8,59 @@ import UI.Panels.SelectDeckPanel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Random;
 
 import static FolderController.CardFolderReader.addFolderStructure;
 
-public class CardContent extends JPanel implements SetDeckName {
+public class CardContent extends JPanel implements SetDeckName, ActionListener {
     private static CardContent instance;
     private String deckName;
-    private File folder;
 
-    private JLabel nameLabel;
-    private CustomTextArea frontTextArea;
-    private CustomBorderTextArea rightTextArea = new CustomBorderTextArea();
-    private CustomBorderTextArea wrong1TextArea = new CustomBorderTextArea();
-    private CustomBorderTextArea wrong2TextArea = new CustomBorderTextArea();
+    private final JLabel nameLabel;
+    private final CustomTextArea frontTextArea;
+    private final CustomBorderTextArea rightTextArea = new CustomBorderTextArea();
+    private final CustomBorderTextArea wrong1TextArea = new CustomBorderTextArea();
+    private final CustomBorderTextArea wrong2TextArea = new CustomBorderTextArea();
 
-    private JButton rightButton = new JButton();
-    private JButton wrongButton = new JButton();
-    private JButton wrong2Button = new JButton();
-    private JPanel namePanel;
-    private JPanel frontCardPanel;
-    private JLabel frontCardLabel;
+    private final JButton rightButton = new JButton();
+    private final JButton wrongButton = new JButton();
+    private final JButton wrong2Button = new JButton();
+    private final JButton shuffleButton = new JButton();
+    private final JButton previousButton = new JButton();
+    private final JButton nextButton = new JButton();
+    private final JPanel frontCardPanel;
+    private final JLabel frontCardLabel;
     private Card[] cards;
-    private int currentIndex = 1;
+    private int currentIndex = 0;
+
+    private final JPanel cardPanel;
+
+    private static class AnswerGroup {
+        CustomBorderTextArea textArea;
+        JButton button;
+
+        AnswerGroup(CustomBorderTextArea textArea, JButton button) {
+            this.textArea = textArea;
+            this.button = button;
+        }
+    }
 
     private CardContent() {
-        SelectDeckPanel selectDeckPanel = SelectDeckPanel.getInstance();
-        deckName = selectDeckPanel.getSelectDeck();
+        deckName = SelectDeckPanel.getSelectDeck();
         setDeckName(deckName);
 
         this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1, true));
         this.setLayout(new BorderLayout());
 
-        JPanel cardPanel = new JPanel();
+        cardPanel = new JPanel();
         GridBagLayout gridBagLayout = new GridBagLayout();
         cardPanel.setLayout(gridBagLayout);
         GridBagConstraints constraints = new GridBagConstraints();
 
-        namePanel = new JPanel();
+        JPanel namePanel = new JPanel();
         namePanel.setLayout(new BorderLayout());
         nameLabel = new JLabel();
         namePanel.add(nameLabel, BorderLayout.PAGE_START);
@@ -63,10 +77,6 @@ public class CardContent extends JPanel implements SetDeckName {
         frontCardPanel.add(frontCardLabel, BorderLayout.PAGE_START);
         frontCardPanel.add(frontTextArea, BorderLayout.CENTER);
 
-
-
-
-
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.gridwidth = 3;
@@ -77,73 +87,140 @@ public class CardContent extends JPanel implements SetDeckName {
 
         constraints.gridy = 1;
         constraints.weighty = 2; // Doppelter Platz in der Höhe
-
         constraints.fill = GridBagConstraints.BOTH;
         cardPanel.add(frontCardPanel, constraints);
-
-        constraints.gridwidth = 1;
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        constraints.weightx = 1; // Gewicht für die Textarea
-        constraints.weighty = 1;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        cardPanel.add(rightTextArea, constraints);
-
-
-        constraints.gridx = 2; // Position des Buttons
-        constraints.gridy = 2;
-        constraints.weightx = 0; // Gewicht für den Button, damit er nicht die ganze Breite einnimmt
-        constraints.fill = GridBagConstraints.NONE;
-        cardPanel.add(rightButton, constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 3;
-        constraints.weightx = 1;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        cardPanel.add(wrong1TextArea, constraints);
-
-        constraints.gridx = 2;
-        constraints.gridy = 3;
-        constraints.weightx = 0;
-        constraints.fill = GridBagConstraints.NONE;
-        cardPanel.add(wrongButton, constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 4;
-        constraints.weightx = 1;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        cardPanel.add(wrong2TextArea, constraints);
-
-        constraints.gridx = 2;
-        constraints.gridy = 4;
-        constraints.weightx = 0;
-        constraints.fill = GridBagConstraints.NONE;
-        cardPanel.add(wrong2Button, constraints);
 
         this.add(cardPanel, BorderLayout.CENTER);
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
 
-        JButton previousButton = new JButton("←");
-        JButton shuffleButton = new JButton("Shuffle");
-        JButton nextButton = new JButton("→");
+        previousButton.setText("←");
+        shuffleButton.setText("Shuffle");
+        nextButton.setText("→");
 
-        previousButton.addActionListener(e -> {
-            if (currentIndex > 0) {
-                currentIndex--;
-                showCard();
+        previousButton.addActionListener(this);
+        nextButton.addActionListener(this);
+        shuffleButton.addActionListener(this);
+
+        buttonPanel.add(previousButton);
+        buttonPanel.add(shuffleButton);
+        buttonPanel.add(nextButton);
+        this.add(buttonPanel, BorderLayout.PAGE_END);
+
+        showCard();
+    }
+
+    public static CardContent getInstance() {
+        if (instance == null) {
+            instance = new CardContent();
+        }
+        return instance;
+    }
+
+    @Override
+    public void setDeckName(String folderName) {
+        deckName = folderName;
+        File folder = new File("./flashyCard_DB/" + folderName);
+        cards = addFolderStructure(folder);
+        currentIndex = 0;
+        showCard();
+        this.revalidate();
+        this.repaint();
+    }
+
+    private void shuffleAnswers() {
+        AnswerGroup[] answerGroups = {
+                new AnswerGroup(rightTextArea, rightButton),
+                new AnswerGroup(wrong1TextArea, wrongButton),
+                new AnswerGroup(wrong2TextArea, wrong2Button)
+        };
+
+        // Shuffle the answers
+        Random rnd = new Random();
+        for (int i = answerGroups.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            AnswerGroup tempGroup = answerGroups[index];
+            answerGroups[index] = answerGroups[i];
+            answerGroups[i] = tempGroup;
+        }
+
+        // Remove existing answers before adding new ones
+        for (AnswerGroup group : answerGroups) {
+            cardPanel.remove(group.textArea);
+            cardPanel.remove(group.button);
+        }
+
+        // Add the TextAreas and Buttons from the shuffled answer groups to the layout
+        for (int i = 0; i < answerGroups.length; i++) {
+            GridBagConstraints constraints = new GridBagConstraints();
+            constraints.gridx = 0;
+            constraints.gridy = 2 + i;
+            constraints.weightx = 1;
+            constraints.fill = GridBagConstraints.HORIZONTAL;
+            cardPanel.add(answerGroups[i].textArea, constraints);
+
+            constraints.gridx = 2;
+            constraints.gridy = 2 + i;
+            constraints.weightx = 0;
+            constraints.fill = GridBagConstraints.NONE;
+            cardPanel.add(answerGroups[i].button, constraints);
+        }
+    }
+
+
+    private void showCard() {
+
+        if (cards != null && currentIndex < cards.length) {
+            Card card = cards[currentIndex];
+            nameLabel.setText(card.getName());
+            frontTextArea.setText(card.getFront());
+            rightTextArea.setText(card.getRight());
+            wrong1TextArea.setText(card.getWrong1());
+
+            shuffleAnswers();
+
+            // Füge das frontTextArea wieder zum Layout hinzu
+            GridBagConstraints constraints = new GridBagConstraints();
+            constraints.gridx = 0;
+            constraints.gridy = 1;
+            constraints.weighty = 2; // Doppelter Platz in der Höhe
+            constraints.fill = GridBagConstraints.BOTH;
+            cardPanel.add(frontCardPanel, constraints);
+
+            wrong2TextArea.setText(card.getWrong2());
+            frontCardLabel.setText("Question of the Card:");
+            frontCardLabel.setFont(frontCardLabel.getFont().deriveFont(Font.BOLD, 14));
+            frontCardLabel.setHorizontalAlignment(JLabel.LEFT);
+            frontCardPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1, true));
+            rightButton.setText("This one");
+            wrongButton.setText("No this one");
+            wrong2Button.setText("Oh wait this one");
+
+
+            // Update the layout
+            cardPanel.revalidate();
+            cardPanel.repaint();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == previousButton) {
+            currentIndex--;
+            if (currentIndex < 0) {
+                currentIndex = 0;
+                shuffleAnswers();
             }
-        });
-
-        nextButton.addActionListener(e -> {
-            if (currentIndex < cards.length - 1) {
-                currentIndex++;
-                showCard();
+            showCard();
+        } else if (e.getSource() == nextButton) {
+            currentIndex++;
+            if (currentIndex >= cards.length) {
+                currentIndex = cards.length - 1;
+                shuffleAnswers();
             }
-        });
-
-        shuffleButton.addActionListener(e -> {
+            showCard();
+        } else if (e.getSource() == shuffleButton) {
             //implementation of the FisherYates shuffle Algorithm
             int numberOfCards = cards.length;
             Random rnd = new Random();
@@ -166,50 +243,6 @@ public class CardContent extends JPanel implements SetDeckName {
             cards = shuffledCard;
             showCard();
             currentIndex = 0;
-        });
-
-        buttonPanel.add(previousButton);
-        buttonPanel.add(shuffleButton);
-        buttonPanel.add(nextButton);
-        this.add(buttonPanel, BorderLayout.PAGE_END);
-    }
-
-    public static CardContent getInstance() {
-        if (instance == null) {
-            instance = new CardContent();
-        }
-        return instance;
-    }
-
-    @Override
-    public void setDeckName(String folderName) {
-        deckName = folderName;
-        folder = new File("./flashyCard_DB/" + folderName);
-        cards = addFolderStructure(folder);
-        currentIndex = 0;
-        showCard();
-        this.revalidate();
-        this.repaint();
-    }
-
-    private void showCard() {
-        if (cards != null && currentIndex < cards.length) {
-            Card card = cards[currentIndex];
-            nameLabel.setText(card.getName());
-            frontTextArea.setText(card.getFront());
-            rightTextArea.setText(card.getRight());
-            wrong1TextArea.setText(card.getWrong1());
-
-            wrong2TextArea.setText(card.getWrong2());
-            frontCardLabel.setText("Question of the Card:");
-            frontCardLabel.setFont(frontCardLabel.getFont().deriveFont(Font.BOLD, 14));
-            frontCardLabel.setHorizontalAlignment(JLabel.LEFT);
-            frontCardPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1, true));
-            rightButton.setText("This one");
-            wrongButton.setText("No this one");
-            wrong2Button.setText("Oh wait this one");
         }
     }
-
-
 }
