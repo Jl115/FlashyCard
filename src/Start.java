188de2ddbc47f22aac7f3ef37c0e25c1fc12d8bc
Jl115 <106701193+Jl115@users.noreply.github.com>
2023-08-07 19:54:30 +0200

    Cleaning up Code and Commenting everything

--- src/Start.java
+++ src/Start.java
@@ -5,7 +5,7 @@ public class Start {
     // Constructor initializes the Menu
     Start() {
         Menu menu = Menu.getInstance();
-        menu.setVisible(true); // Makes the menu visible
+        menu.setVisible(true); // Makes the Menu visible
     }
 
     // Main method, the entry point of the application
